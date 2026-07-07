538fa8376c5796c908384c22d0d36c4328117066
Yug <patelyugdilipbhai@gmail.com>
2026-07-07 15:23:07 +1000

    Updated on the structure

--- 1.1P/js/scripts.js
+++ 1.1P/js/scripts.js
@@ -5,5 +5,5 @@ console.log("Index: ", number)
 document.getElementById("heading").innerHTML= textsArray[number];
 }
 function getRandomNumberBetween(min,max){
-returnMath.floor(Math.random()*(max-min+1)+min);
+return Math.floor(Math.random()*(max-min+1)+min);
 }
